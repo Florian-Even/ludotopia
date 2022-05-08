3b8e3e76c3def046922b10ff5ba3e533eaaaeda6
Florian Even <florianeven.35+1@outlook.fr>
2022-05-09 00:28:55 +0200

    change api to users app

--- users_app/config/application.rb
+++ users_app/config/application.rb
@@ -18,7 +18,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ludotopia
+module UsersApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
